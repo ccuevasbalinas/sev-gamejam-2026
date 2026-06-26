4449110062160904b19c21d66f50b374da4db175
Gonso <gonza7108@gmail.com>
2026-06-26 20:43:22 +0200

    Input System Defined

---
+++ Assets/Scripts/Runtime/Character/CharacterController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterController : MonoBehaviour
+{
+    public CharacterMovement PlayerMovement;
+    
+    public InputActionAsset PlayerActionAsset;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
