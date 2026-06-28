d1c3f8b95912ab4f85d87234dae2776ceaabf09d
ccuevasbalinas <ccuevasbalinas@gmail.com>
2026-06-28 11:17:43 +0200

    Fix switch of post process

--- Assets/Scripts/Runtime/Utils/ActiveSelfSwitcher.cs
+++ Assets/Scripts/Runtime/Utils/ActiveSelfSwitcher.cs
@@ -7,13 +7,25 @@ namespace Runtime.Utils
         [SerializeField] private GameObject obj1;
         [SerializeField] private GameObject obj2;
 
+        private int currentActive = 0;
+
         public void Switch()
         {
             if (obj1 == null || obj2 == null)
                 return;
 
-            obj1.SetActive(!obj2.activeSelf);
-            obj2.SetActive(!obj1.activeSelf);
+            if (currentActive == 0) 
+            {
+                obj1.SetActive(false);
+                obj2.SetActive(true);
+            }
+            else
+            {
+                obj1.SetActive(true);
+                obj2.SetActive(false);
+            }
+
+            currentActive = currentActive == 1 ? 0 : 1;
         }
     }
 }
